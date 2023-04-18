6c192e6251f67164b198854890062f58d0080b70
Jared Howland <jaredhowland@users.noreply.github.com>
2023-04-18 08:29:40 -0600

    Filter input

--- generate.php
+++ generate.php
@@ -7,7 +7,7 @@ $template = new Template();
 
 $template->tm('header', 'Barcode Generator');
 
-$queryString = htmlspecialchars($_SERVER['QUERY_STRING'], ENT_QUOTES);
+$queryString = filter_var($_SERVER['QUERY_STRING'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 echo '<p><a href="barcodes-txt.php?'.$queryString.'">Download plain text file to send to LIT for creation of pseudo-patrons</a></p>';
 echo '<p><a href="barcodes.php?'.$queryString.'">Download PDF file of barcodes</a></p>';
 
